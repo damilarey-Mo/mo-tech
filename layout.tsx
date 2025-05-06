e0990aeea753e6a1842a5e7a174d1f76734b017c
dareyMo <austin.clark5089@gmail.com>
2025-05-06 19:12:39 +0100

    update layout main

--- layout.tsx
+++ layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "./theme-provider";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
