3d361b0ecaefcd2e1ff52a5203d6592cde059d10
dareyMo <austin.clark5089@gmail.com>
2025-05-06 19:09:44 +0100

    app/layout import fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/app/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
